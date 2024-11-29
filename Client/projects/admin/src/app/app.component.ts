21c325a053898c336dcfe75ff38f9ed0c2525158
Mahmoud Hassan <mahmoudd.a.hassan@gmail.com>
2024-11-29 08:02:04 +0300

    add login , header components with install mdb bootstrap

--- Client/projects/admin/src/app/app.component.ts
+++ Client/projects/admin/src/app/app.component.ts
@@ -1,32 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatInputModule } from '@angular/material/input';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { LoginComponent } from './features/auth/components/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    MatListModule,
-    MatSidenavModule,
-    RouterOutlet,
-    MatButtonModule,
-    MatToolbarModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule,
-    MatDividerModule,
-    MatButtonToggleModule,
-    MatCheckboxModule,
-  ],
+  imports: [HeaderComponent, RouterOutlet , LoginComponent
+  ], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
--- Client/projects/admin/src/app/shared/components/header/header.component.ts
+++ Client/projects/admin/src/app/shared/components/header/header.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router'; // Import RouterModule if you are using router directives
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule], // Import RouterModule to use router directives
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}

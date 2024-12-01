eb3d8e365117921733913a9a58c8be9e34fae85c
Mahmoud Hassan <mahmoudd.a.hassan@gmail.com>
2024-12-01 09:28:25 +0300

    handle login logic in LoginComponent

--- Client/projects/admin/src/app/features/auth/components/login/login.component.ts
+++ Client/projects/admin/src/app/features/auth/components/login/login.component.ts
@@ -1,27 +1,47 @@
+import { LoginService } from './../../services/login.service';
 import { Component } from '@angular/core';
-import { FormBuilder , FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  loginFrom  : FormGroup
-  constructor(private fb : FormBuilder) { 
+  loginForm: FormGroup;
 
-    this.loginFrom  = this.fb.group({ 
-      email : ['' , [Validators.required , Validators.email]],
-      password : ['' , Validators.required]
-    
-    })
+  constructor(private fb: FormBuilder, private loginService: LoginService) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
   }
+
   onSubmit() {
-    console.log(this.loginFrom.value);
-    
+    if (this.loginForm.valid) {
+      this.loginService.login(
+        this.loginForm.value.email,
+        this.loginForm.value.password
+      ).subscribe({
+        next: (response) => {
+          console.log(response);
+        },
+        error: (error) => {
+          console.error(error);
+        },
+        complete: () => {
+          console.log('Login completed');
+        }
+      });
+    } else {
+      console.error('Form is invalid');
+    }
   }
-
 }
--- Client/projects/admin/src/app/features/auth/services/login.service.ts
+++ Client/projects/admin/src/app/features/auth/services/login.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  url = "/login"
+  constructor(private http : HttpClient) {
 
-  constructor() { }
+   }
+   login(email :string , password : string) : Observable<any> {
+    console.log( email ,   password);
+    
+    return this.http.post(this.url , {email , password})
+   }
 }

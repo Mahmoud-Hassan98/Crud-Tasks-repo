e5e10cf3c44ec3c92f29cb4d193aca9de8731a1f
Mahmoud Hassan <mahmoudd.a.hassan@gmail.com>
2024-12-01 08:17:22 +0300

    add some logic in login component

--- Client/projects/admin/src/app/features/auth/components/login/login.component.ts
+++ Client/projects/admin/src/app/features/auth/components/login/login.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder , FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  
+  loginFrom  : FormGroup
+  constructor(private fb : FormBuilder) { 
+
+    this.loginFrom  = this.fb.group({ 
+      email : ['' , [Validators.required , Validators.email]],
+      password : ['' , Validators.required]
+    
+    })
+  }
+  onSubmit() {
+    console.log(this.loginFrom.value);
+    
+  }
+
 }

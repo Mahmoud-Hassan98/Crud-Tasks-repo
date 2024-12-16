8e5d232ddf7594a98030fcbf8cf4056cbc149c8a
Mahmoud Hassan <mahmoudd.a.hassan@gmail.com>
2024-12-16 04:30:14 +0300

    add routes

--- Client/projects/admin/src/app/app.component.ts
+++ Client/projects/admin/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
-import { LoginComponent } from './features/auth/components/login/login.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet , LoginComponent
+  imports: [HeaderComponent, RouterOutlet , FooterComponent
   ], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
--- Client/projects/admin/src/app/app.routes.ts
+++ Client/projects/admin/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/components/login/login.component';
+import { ManageUsersComponent } from './features/manage-users/components/manage-users/manage-users.component';
+import { ManageTasksComponent } from './features/manage-tasks/components/manage-tasks/manage-tasks.component';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent },
-  ];
-  
+  { path: '', redirectTo: 'tasks', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'users', component: ManageUsersComponent },
+  { path: 'tasks', component: ManageTasksComponent },
+];

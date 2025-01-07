5383e94b052535888b38a0d67e84494f5eeb6e82
Mahmoud Hassan <mahmoudd.a.hassan@gmail.com>
2025-01-07 08:50:23 +0300

    add spinner, toaster, test table for tasks component

--- Client/projects/admin/src/app/app.config.ts
+++ Client/projects/admin/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations'; 
-
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
@@ -10,6 +10,11 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    provideAnimations(), 
+    provideAnimations(),
+    provideToastr({
+      timeOut: 3000, // Default duration for toast messages
+      positionClass: 'toast-top-right', // Position of the toast
+      preventDuplicates: true, // Prevent duplicate toasts
+    }),
   ],
 };
--- Client/projects/admin/src/app/features/auth/components/login/login.component.ts
+++ Client/projects/admin/src/app/features/auth/components/login/login.component.ts
@@ -1,64 +1,74 @@
 import { CommonModule } from '@angular/common';
 import { LoginService } from './../../services/login.service';
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from 'ngx-spinner';
 import {
   FormBuilder,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgxSpinnerModule } from 'ngx-spinner';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule , CommonModule],
+  imports: [ReactiveFormsModule, CommonModule , NgxSpinnerModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private loginService: LoginService) {
+  constructor(
+    private fb: FormBuilder,
+    private loginService: LoginService,
+    private toaster: ToastrService,
+    private router: Router,
+    private spinner: NgxSpinnerService
+  ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
     });
     this.loginForm.valueChanges.subscribe((value: any) => {
-console.log(value);
-
-    })
-    
+      console.log(value);
+    });
   }
-  get email()
-    {
+  get email() {
     return this.loginForm.get('email');
   }
-  get password()
-    {
+  get password() {
     return this.loginForm.get('password');
   }
-      
+
   onSubmit() {
     if (this.loginForm.valid) {
-      this.loginService.login(
-        this.loginForm.value.email,
-        this.loginForm.value.password
-      ).subscribe({
-        next: (response) => {
-          console.log(response);
-        },
-        error: (error) => {
-          console.error(error);
-        },
-        complete: () => {
-          console.log('Login completed');
-        }
-      });
+      this.loginService
+        .login(this.loginForm.value.email, this.loginForm.value.password)
+        .subscribe({
+          next: (response) => {
+            this.toaster.success('success', 'Login Success');
+            this.spinner.show();
+            this.router.navigate(['/tasks']);
+            this.spinner.hide();
+            console.log(response);
+          },
+          error: (error) => {
+            this.spinner.hide();
+            this.toaster.error(error.error);
+            this.spinner.hide();
+            console.error(error);
+          },
+          complete: () => {
+            console.log('Login completed');
+          },
+        });
     } else {
       console.error('Form is invalid');
     }
   }
-
-  
 }

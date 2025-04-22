b29044633b366cbf0f56e249e65da72e2b2c3c0b
Mahmoud-Hassan98 <126132249+Mahmoud-Hassan98@users.noreply.github.com>
2025-04-22 05:18:55 +0300

    add test java server

---
+++ to-do-task/src/test/java/com/crud/to_do_task/ToDoTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.to_do_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

6731c745316690f992be404e3d4c7851d7e0787b
Mahmoud-Hassan98 <126132249+Mahmoud-Hassan98@users.noreply.github.com>
2025-04-23 08:42:56 +0300

    add controller , model , service to java server

---
+++ to-do-task/src/main/java/com/crud/to_do_task/controller/TaskController.java
@@ -0,0 +1,24 @@
+package com.crud.to_do_task.controller;
+
+import com.crud.to_do_task.model.entity.Task;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
+
+
+public class TaskController {
+
+
+    @PostMapping("/add-task")
+    public Task createTask(@RequestBody Task task){
+        System.out.println(5555);
+
+        return task;
+    }
+
+
+}
---
+++ to-do-task/src/main/java/com/crud/to_do_task/model/entity/Task.java
@@ -0,0 +1,39 @@
+package com.crud.to_do_task.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Table
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private String userId;
+
+    @Temporal(TemporalType.DATE)
+    @com.fasterxml.jackson.annotation.JsonFormat(shape = com.fasterxml.jackson.annotation.JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date deadline;
+
+    private String description;
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", userId='" + userId + '\'' +
+                ", deadline=" + deadline +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}

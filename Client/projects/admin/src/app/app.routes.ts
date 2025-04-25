62d4bb717f50fce10c3506e434ff50a76f2b1050
Mahmoud-Hassan98 <126132249+Mahmoud-Hassan98@users.noreply.github.com>
2025-04-25 05:11:13 +0300

    add not found page

--- Client/projects/admin/src/app/app.routes.ts
+++ Client/projects/admin/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/components/login/login.component';
 import { ManageUsersComponent } from './features/manage-users/components/manage-users/manage-users.component';
 import { ManageTasksComponent } from './features/manage-tasks/components/manage-tasks/manage-tasks.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'tasks', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'users', component: ManageUsersComponent },
   { path: 'tasks', component: ManageTasksComponent },
+  { path: 'tasks', component: NotFoundComponent },
 ];
